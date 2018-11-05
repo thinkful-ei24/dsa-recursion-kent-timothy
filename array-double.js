af4452f2567e17b37eb143b4604524d7621fea3b
Timothy <superfluous11@gmail.com>
2018-11-05 08:38:22 -0800

    completed array double recursive function

---
+++ array-double.js
@@ -0,0 +1,14 @@
+// input: array of numbers
+// output: array of numbers
+
+function arrayDouble(array){
+  // base case
+  if(!array.length){
+    return [];
+  } else {
+    //general case
+    return [ array[0] * 2, ...arrayDouble(array.slice(1))];
+  }
+}
+
+console.log(arrayDouble([1, 2, 3]));
